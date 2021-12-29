db66889c2186624bee8e67aab4d5d2976d5e7745
jwiergowska <85780659+jwiergowska@users.noreply.github.com>
2021-12-29 18:31:41 +0100

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,11 +7,19 @@ const Footer = () => {
       <hr style={{ width: "90%", marginTop: 20 }} />
       <span className="name">
         Made by{" "}
-        <a href="https://github.com/jwiergowska/jwiergowska">
+        <a href="" target="__blank">
           Julia Wiergowska
         </a>
       </span>
-     
+      <div className="iconContainer">
+        <a href="" target="__blank">
+          <i className="fab fa-instagram-square fa-2x"></i>
+        </a>
+        <a href="" target="__blank">
+          <i className="fab fa-linkedin fa-2x"></i>
+        </a>
+        <a href="" target="__blank">
+          <i className="fab fa-youtube fa-2x"></i>
         </a>
       </div>
     </div>
